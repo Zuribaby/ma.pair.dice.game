12ab68f8514e62bdcb5cf4765c5d76b272584484
Zuribaby <chiamakanwobod07@gmail.com>
2024-03-11 08:27:45 -0700

    update

--- script.js
+++ script.js
@@ -4,15 +4,15 @@ const dice = document.getElementById('dice-btn');
 
 function diceGame() {
 	//DICE 1
-	var randomDice = Math.floor((Math.random() * 6) + 1);
-	var diceLocation = "img/" + "Dice-" + randomDice + ".png";
-	var img1 = document.querySelectorAll("img")[0];
+	let randomDice = Math.floor((Math.random() * 6) + 1);
+	let diceLocation = "img/" + "Dice-" + randomDice + ".png";
+	let img1 = document.querySelectorAll("img")[0];
 	img1.setAttribute("src", diceLocation);
 	
 	//DICE 2
-	var randomDice2 = Math.floor((Math.random() * 6) + 1);
-	var diceLocation2 = "img/" + "Dice-" + randomDice2 + ".png";
-	var img2 = document.querySelectorAll("img")[1];
+	let randomDice2 = Math.floor((Math.random() * 6) + 1);
+	let diceLocation2 = "img/" + "Dice-" + randomDice2 + ".png";
+	let img2 = document.querySelectorAll("img")[1];
 	img2.setAttribute("src", diceLocation2);
 
 	// Compares two random dice values and updates the HTML content to declare the winner, indicate a draw, or display an error message along with a prompt to play again.
