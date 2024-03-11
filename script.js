ef3ccf2e34fe482676f4012321fe531aa25833d1
Thabang Mokubedi <153010329+Thabang-Mokubedi@users.noreply.github.com>
2024-03-11 19:46:44 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+const dice = document.getElementById('dice-btn');
+
+// Generates a random dice value, updates the corresponding image source in the HTML, and represents the first dice in a simple dice game.
+
+function diceGame() {
+	//DICE 1
+	var randomDice = Math.floor((Math.random() * 6) + 1);
+	var diceLocation = "img/" + "Dice-" + randomDice + ".png";
+	var img1 = document.querySelectorAll("img")[0];
+	img1.setAttribute("src", diceLocation);
+	
+	//DICE 2
+	var randomDice2 = Math.floor((Math.random() * 6) + 1);
+	var diceLocation2 = "img/" + "Dice-" + randomDice2 + ".png";
+	var img2 = document.querySelectorAll("img")[1];
+	img2.setAttribute("src", diceLocation2);
+
+	// Compares two random dice values and updates the HTML content to declare the winner, indicate a draw, or display an error message along with a prompt to play again.
+
+	if (randomDice > randomDice2) {
+		document.querySelector("h1").innerHTML = " &#128681; Player 1 Win !";
+	}
+	else if (randomDice < randomDice2) {
+		document.querySelector("h1").innerHTML = "Player 2 Win ! &#128681; ";
+	}
+	else if (randomDice = randomDice2) {
+		document.querySelector("h1").innerHTML = "&#128681; Draw! &#128681;";
+	}
+	else {
+		document.querySelector("h1").innerHTML = "Something Error!"
+	}
+	dice.innerHTML = "Play Again"
+}
